64ce952a5320a915438dfe63591059b20191127f
otmanabdoun <64623609+otmanabdoun@users.noreply.github.com>
2021-11-16 03:04:01 +0100

    Add files via upload

---
+++ Sqlite.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  3 02:45:11 2021
+
+@author: User
+"""
+
+import sqlite3
+
+connexion = sqlite3.connect("dbM2IQL.db")
+curseur = connexion.cursor()  
+
+curseur.execute('''DROP TABLE Etudiant''')
+
+curseur.execute('''CREATE TABLE IF NOT EXISTS Etudiant(
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    Nom TEXT,
+    Master TEXT
+)''')
+
+curseur.execute("""DELETE FROM Etudiant""")
+
+donnees = [("El Fahssi", "MQL"), ("Ktiti Mohamed", "MQL"), ("Amakran Hajar", "M2I")]
+#Exécutions multiples
+for donnee in donnees:
+    curseur.execute('''INSERT INTO Etudiant (Nom, Master) VALUES (?, ?)''', donnee)
+connexion.commit()
+
+print(" ---- Liste des étudiants inscrits en Masters Informatique : MQL et M2I ---- \n")
+curseur.execute("SELECT * FROM Etudiant")
+result = curseur.fetchone()
+while result:
+    print(result)
+    result = curseur.fetchone()
+
+
+donnee=("M2I", )
+curseur.execute("SELECT * FROM Etudiant WHERE Master = ?", donnee)
+result = curseur.fetchone()
+while result:
+    print(result)
+    result = curseur.fetchone()
