cf99cbd3f6ee87a5c2b96887e47a784617dad390
otmanabdoun <64623609+otmanabdoun@users.noreply.github.com>
2026-01-22 09:22:17 +0100

    Add files via upload

---
+++ 1 - Programmation Java/File WITH/with.py
@@ -0,0 +1,23 @@
+
+with open("truc.txt", "w", encoding='utf8') as f:
+    s = 'MASTERS  QL & 2I \n'
+    f.write(s) 
+    l = [' IHM ', ' ML ', ' WEB ']
+    f.writelines(l)    
+
+with open("truc2.txt", "w", encoding='utf8') as f2:
+    f2 = open("truc2.txt", "w", encoding='utf8')
+    print("Les Modules des deux masters", file=f2)
+
+with open("truc.txt", "r", encoding='utf8') as f:
+    s = f.read()
+    print(s)
+
+with open("truc.txt", "r", encoding='utf8') as f:
+    s = f.readlines()
+    print(s)
+
+
+
+x = pickle.load(f)
+pickle.dump(x, f)
---
+++ 1 - Programmation Java/Héritage/Poly.py
@@ -0,0 +1,19 @@
+﻿
+class Master: 
+          def ModuleWEB(self): 
+                 print("Développement Web") 
+class M2I(Master):
+           def ModuleWEB(self): 
+                  print("Web Sémantique") 
+class MQL(Master): 
+           def ModuleWEB(self): 
+                  print("PHP OO")
+
+E=Master()
+E.ModuleWEB()
+
+E=M2I()
+E.ModuleWEB()
+
+E=MQL()
+E.ModuleWEB()
